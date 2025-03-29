59cf81ee9c8532a8b3ab43070f0eefe11899c864
Brandon Saunders <137465318+Halcynixa@users.noreply.github.com>
2025-03-29 11:37:42 -0700

    Update part3_q1.cpp

--- part3_q1.cpp
+++ part3_q1.cpp
@@ -1,1 +1,14 @@
-// TBA
+#include <iostream>
+using namespace std;
+
+int main() {
+	string name, age, number;
+	cout << "Enter your name: \n";
+	cin >> name;
+	cout << "Enter your age: \n";
+	cin >> age;
+	cout << "Enter your favorite number: \n";
+	cin >> number;
+	cout << "------\nName: " << name << "\nAge: " << age << "\nFavorite number: " << number;
+	return 0;
+}
