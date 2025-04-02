f2261bec1152c4ff625ffce101ba570768eb7cc1
Brandon Saunders <137465318+Halcynixa@users.noreply.github.com>
2025-04-01 18:39:50 -0700

    Update part4_q2.cpp

--- part4_q2.cpp
+++ part4_q2.cpp
@@ -1,1 +1,14 @@
-//TBA
+#include<iostream>
+using namespace std;
+
+int main() {
+	int year;
+	cout << "Enter year: \n";
+	cin >> year;
+	if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+		cout << year << " is a leap year.\n";
+	}
+	else {
+		cout << year << " is not a leap year.\n";
+	}
+}
