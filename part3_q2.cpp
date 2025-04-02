546918371c0d50065536c9e6bbbac5cb76b13cad
Brandon Saunders <137465318+Halcynixa@users.noreply.github.com>
2025-04-02 16:38:56 -0700

    Update part3_q2.cpp

--- part3_q2.cpp
+++ part3_q2.cpp
@@ -1,1 +1,11 @@
-// TBA
+#include<iostream>
+using namespace std;
+
+int main() {
+	int i = 4; // integer
+	float f = 3.14; // decimal number
+	char c = 'B'; // single character
+	string s = "hello"; //several characters
+	cout << i << "\n" << f << "\n" << c << "\n" << s;
+	return 0;
+}
