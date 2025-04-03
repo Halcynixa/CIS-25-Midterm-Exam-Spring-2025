7b3eb77ea4ad53cdb03eb80e98eaf5c8cf168715
Brandon Saunders <137465318+Halcynixa@users.noreply.github.com>
2025-04-03 15:32:05 -0700

    Update part2_q2.cpp

--- part2_q2.cpp
+++ part2_q2.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 int main() {
 	string array [10]; // declare array size of 10
-	int x = 0; // set value for while loop
-	string input; // set value for cin
+	int x = 0;
+	string input;
 	while (x < 10) {
 		cout << "Enter something until array is full: \n";
 		cin >> input;
-		array[x] = input;
-		x += 1;
+		array[x] = input; // store array in element #x
+		x += 1; // increment x
 	}
 	cout << "----\n";
 	for (int i = 0; i < 10; i++) {
