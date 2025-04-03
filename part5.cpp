8304cbd13d1615562b960d60a49d88b5b0dd292d
Brandon Saunders <137465318+Halcynixa@users.noreply.github.com>
2025-04-03 15:36:26 -0700

    Update part5.cpp

--- part5.cpp
+++ part5.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 int main() {
-	float x, z;
-	char op;
+	float x, z; // 1st and 2nd digit
+	char op; // operand (+ - * /)
 	cout << "Enter first digit: \n";
 	cin >> x;
 	cout << "Enter operand (+ - * /): \n";
@@ -12,19 +12,19 @@ int main() {
 	cin >> z;
 	cout << "---------\n";
 	switch (op) {
-	case '+':
+	case '+': // addition (+)
 		cout << x + z;
 		break;
-	case '-':
+	case '-': // subtraction (-)
 		cout << x - z;
 		break;
-	case '*':
+	case '*': // multiplication (*)
 		cout << x * z;
 		break;
-	case '/':
+	case '/': // division (/)
 		cout << x / z;
 		break;
 	default:
-		cout << "This calcuator does not support that operand.";
+		cout << "This calcuator does not support that operand."; // operand was something else
 	}
 }
