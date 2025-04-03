ecd765d7c6fafa59f8efcc1589c4a51d7ce22b8f
Brandon Saunders <137465318+Halcynixa@users.noreply.github.com>
2025-04-02 17:41:57 -0700

    Update part5.cpp

--- part5.cpp
+++ part5.cpp
@@ -1,1 +1,30 @@
-//TBA
+#include<iostream>
+using namespace std;
+
+int main() {
+	float x, z;
+	char op;
+	cout << "Enter first digit: \n";
+	cin >> x;
+	cout << "Enter operand (+ - * /): \n";
+	cin >> op;
+	cout << "Enter second digit: \n";
+	cin >> z;
+	cout << "---------\n";
+	switch (op) {
+	case '+':
+		cout << x + z;
+		break;
+	case '-':
+		cout << x - z;
+		break;
+	case '*':
+		cout << x * z;
+		break;
+	case '/':
+		cout << x / z;
+		break;
+	default:
+		cout << "This calcuator does not support that operand.";
+	}
+}
