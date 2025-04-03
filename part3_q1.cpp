d9b053f4dbdef6eefdccdb2543f13df3d41f89a0
Brandon Saunders <137465318+Halcynixa@users.noreply.github.com>
2025-04-03 15:32:52 -0700

    Update part3_q1.cpp

--- part3_q1.cpp
+++ part3_q1.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 int main() {
-	string name, age, number;
+	string name, age, number; // declare all needed variables
 	cout << "Enter your name: \n";
 	cin >> name;
 	cout << "Enter your age: \n";
 	cin >> age;
 	cout << "Enter your favorite number: \n";
 	cin >> number;
-	cout << "------\nName: " << name << "\nAge: " << age << "\nFavorite number: " << number;
+	cout << "------\nName: " << name << "\nAge: " << age << "\nFavorite number: " << number; // print
 	return 0;
 }
