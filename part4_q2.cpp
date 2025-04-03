10e3bcc9be8b34b253bd9a39e6af54147d468a26
Brandon Saunders <137465318+Halcynixa@users.noreply.github.com>
2025-04-03 15:35:18 -0700

    Update part4_q2.cpp

--- part4_q2.cpp
+++ part4_q2.cpp
@@ -5,7 +5,7 @@ int main() {
 	int year;
 	cout << "Enter year: \n";
 	cin >> year;
-	if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+	if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) { // check if year is divisible by 4 and is not disivible by 100 unless it's also divisible by 400 (definition of a leap year)
 		cout << year << " is a leap year.\n";
 	}
 	else {
