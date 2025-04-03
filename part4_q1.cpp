81fd309b9f7d49dc2e6e5f4c8258731690d5d1a9
Brandon Saunders <137465318+Halcynixa@users.noreply.github.com>
2025-04-03 15:34:09 -0700

    Update part4_q1.cpp

--- part4_q1.cpp
+++ part4_q1.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-	int x, y, z;
+	int x, y, z; // x = first number, y = second number, z = third number
 	cout << "Enter first number: \n";
 	cin >> x;
 	cout << "Enter second number: \n";
@@ -11,17 +11,17 @@ int main() {
 	cin >> z;
 	if (x >= y) {
 		if (x >= z) {
-			cout << "Largest number: " << x;
+			cout << "Largest number: " << x; // x > y and z
 		}
 	}
 	if (y >= x) {
 		if (y >= z) {
-			cout << "Largest number: " << y;
+			cout << "Largest number: " << y; // y > x and z
 		}
 	}
 	if (z >= x) {
 		if (z >= y) {
-			cout << "Largest number: " << z;
+			cout << "Largest number: " << z; // z > x and y
 		}
 	}
 }
