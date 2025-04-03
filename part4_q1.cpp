7d28cb82972c098ff7413b4570ad6c6ffed85e9f
Brandon Saunders <137465318+Halcynixa@users.noreply.github.com>
2025-04-02 17:25:20 -0700

    Update part4_q1.cpp

--- part4_q1.cpp
+++ part4_q1.cpp
@@ -1,1 +1,27 @@
-// TBA
+#include<iostream>
+using namespace std;
+
+int main() {
+	int x, y, z;
+	cout << "Enter first number: \n";
+	cin >> x;
+	cout << "Enter second number: \n";
+	cin >> y;
+	cout << "Enter third number: \n";
+	cin >> z;
+	if (x >= y) {
+		if (x >= z) {
+			cout << "Largest number: " << x;
+		}
+	}
+	if (y >= x) {
+		if (y >= z) {
+			cout << "Largest number: " << y;
+		}
+	}
+	if (z >= x) {
+		if (z >= y) {
+			cout << "Largest number: " << z;
+		}
+	}
+}
